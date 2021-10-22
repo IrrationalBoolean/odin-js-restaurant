c43691d010d320a3bf741ee0c27120361ac78810
KJ <irateboolean@gmx.com>
2021-10-22 18:39:06 -0400

    added main div

--- src/index.js
+++ src/index.js
@@ -5,5 +5,10 @@ import footer from './footer';
 import nav from './nav';
 
 
+const body = document.querySelector('body')
+const div = document.createElement('div')
+
 nav()
+div.setAttribute('id', 'main')
+body.appendChild(div)
 footer()
