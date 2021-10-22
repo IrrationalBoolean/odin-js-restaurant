ce2beec35602efba420f244a4c982c5e2250dd20
KJ <irateboolean@gmx.com>
2021-10-22 17:20:19 -0400

    added github link to footer

--- src/footer.js
+++ src/footer.js
@@ -3,6 +3,11 @@ import './footer.css';
 export default function footer(){
   const body = document.querySelector('body')
   const footer = document.createElement('footer')
-  footer.innerText = '© 2021 IrrationalBoolean'
+  const link = document.createElement('a')
+  footer.innerText = '© 2021 '
+  link.innerText = 'IrrationalBoolean'
+  link.href = 'https://github.com/IrrationalBoolean'
+  link.classList.add('footer-text')
+  footer.appendChild(link)
   body.appendChild(footer)
 }
