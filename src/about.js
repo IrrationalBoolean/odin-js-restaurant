88dd8556e7e5f58bb820543485c8f9c9572a136a
KJ <irateboolean@gmx.com>
2021-10-23 00:28:35 -0400

    added about

--- src/about.js
+++ src/about.js
@@ -9,7 +9,28 @@ export default function makeAbout() {
   console.log('about')
 
   const content = document.createElement("div")
+  const header = document.createElement("h1")
+  const img1 = document.createElement("img")
+  const par1 = document.createElement("p")
+
+  header.textContent = "About Us"
+  img1.src = "https://unsplash.it/700/700"
+  par1.textContent = "We started in 2005 with a dream and a loan, now 16 years later we continue to pay the loan, and our dreams have been crushed.  At least we still have this restaurant.  Come and enjoy our delicious delicacies, from peanut butter ice cream to surft and turf, if we don't have what you want, then order something else!  We've got bills to pay and mouths to feed, so eat up and give me your money."
+
+  header.classList.add("about-header")
   content.classList.add("about")
+  img1.classList.add("about-img")
+  par1.classList.add("about-text")
+
+  content.appendChild(header)
+  content.appendChild(img1)
+  content.appendChild(par1)
+
+  
+
 
   main.appendChild(content)
 }
+
+
+
